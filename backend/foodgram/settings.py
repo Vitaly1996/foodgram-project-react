be8008758c85327674368a40ae4ef9f9d12ef81f
Vitalii Sergeev <100026202+Vitaly1996@users.noreply.github.com>
2022-11-22 21:37:09 +0300

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -2,10 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv(
-    'SECRET_KEY',
-    default='2)_zi3!44h=%ob3-m!^$z2(6gz39bw!jz8^)*=a1r6^ll(mk9i'
-)
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
